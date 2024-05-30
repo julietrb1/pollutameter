eee766b05c3d17a9a31cc17b2c5cbc2efe60fb87
Juliet Boyd <5061312+julietrb1@users.noreply.github.com>
2024-05-30 23:09:39 +1000

    Make frequencies and codes read-only

--- Pollutameter.Api/Naq/NaqParameter.cs
+++ Pollutameter.Api/Naq/NaqParameter.cs
@@ -13,12 +13,12 @@ public static class NaqParameterFrequency
 {
     public const string AverageForHour = "Hourly average";
     public const string AverageFor24H = "24h rolling average derived from 1h average";
-    public static IReadOnlySet<string> AllFrequencies = new HashSet<string>([AverageForHour, AverageFor24H]);
+    public static readonly IReadOnlySet<string> AllFrequencies = new HashSet<string>([AverageForHour, AverageFor24H]);
 }
 
 public static class NaqParameterCode
 {
     public const string Pm25 = "PM2.5";
     public const string Pm10 = "PM10";
-    public static IReadOnlySet<string> AllCodes = new HashSet<string>([Pm25, Pm10]);
+    public static readonly IReadOnlySet<string> AllCodes = new HashSet<string>([Pm25, Pm10]);
 }
