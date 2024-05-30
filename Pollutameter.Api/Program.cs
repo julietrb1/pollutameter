25c01dff39eedce1926b7e586adab1af154acb7a
Juliet Boyd <5061312+julietrb1@users.noreply.github.com>
2024-05-30 22:26:56 +1000

    Configure Swagger

--- Pollutameter.Api/Program.cs
+++ Pollutameter.Api/Program.cs
@@ -6,18 +6,19 @@ using Pollutameter.Api.Response;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
-
 // Add AWS Lambda support. When application is run in Lambda Kestrel is swapped out as the web server with Amazon.Lambda.AspNetCoreServer. This
 // package will act as the webserver translating request and responses between the Lambda event source and ASP.NET Core.
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
-
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 var naqApi = new NaqApi();
 
 app.MapGet("/air-quality", async (double latitude, double longitude, double maxKm = 10) =>
